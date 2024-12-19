cc2980818a4267c5ca45d69ddc3281d1a6bb84ac
Murtaza Amin <murtazaamin1@gmail.com>
2024-12-18 20:11:35 -0700

    Removed extraneous loading code.

--- src/Objects/Ships/Earthling/A1/EarthlingA1.py
+++ src/Objects/Ships/Earthling/A1/EarthlingA1.py
@@ -9,27 +9,3 @@ class EarthlingA1(Projectile):
 
     def __init__(self, parent):
         super().__init__("EarthlingA1", parent)
-
-        if EarthlingA1.shared_sprites is None:
-            # Load sprites
-            EarthlingA1.shared_sprites = []
-            for i in range(Const.SHIP_DIRECTIONS):
-                sprite_path = self.sprite_location.joinpath(f'{self.name}{i:02d}.png')
-                EarthlingA1.shared_sprites.append(pygame.image.load(str(sprite_path)).convert_alpha())
-
-            # Load death animation if it exists
-            if self.death_anim > 0:
-                EarthlingA1.death_anim = []
-                for i in range(self.death_anim):
-                    try:
-                        death_path = self.sprite_location.joinpath(f'{self.name}die{i:02d}.png')
-                        EarthlingA1.death_anim.append(pygame.image.load(str(death_path)).convert_alpha())
-                    except pygame.error:
-                        break
-
-            # Load sound if it exists
-            try:
-                sound_path = self.sprite_location.joinpath(f'{self.name}.wav')
-                EarthlingA1.launch_sound = pygame.mixer.Sound(str(sound_path))
-            except pygame.error:
-                EarthlingA1.launch_sound = None
--- src/Objects/Ships/KzerZa/A1/KzerZaA1.py
+++ src/Objects/Ships/KzerZa/A1/KzerZaA1.py
@@ -9,27 +9,3 @@ class KzerZaA1(Projectile):
 
     def __init__(self, parent):
         super().__init__("KzerZaA1", parent)
-
-        if KzerZaA1.shared_sprites is None:
-            # Load sprites
-            KzerZaA1.shared_sprites = []
-            for i in range(Const.SHIP_DIRECTIONS):
-                sprite_path = self.sprite_location.joinpath(f'{self.name}{i:02d}.png')
-                KzerZaA1.shared_sprites.append(pygame.image.load(str(sprite_path)).convert_alpha())
-
-            # Load death animation if it exists
-            if self.death_anim > 0:
-                KzerZaA1.death_anim = []
-                for i in range(self.death_anim):
-                    try:
-                        death_path = self.sprite_location.joinpath(f'{self.name}die{i:02d}.png')
-                        KzerZaA1.death_anim.append(pygame.image.load(str(death_path)).convert_alpha())
-                    except pygame.error:
-                        break
-
-            # Load sound if it exists
-            try:
-                sound_path = self.sprite_location.joinpath(f'{self.name}.wav')
-                KzerZaA1.launch_sound = pygame.mixer.Sound(str(sound_path))
-            except pygame.error:
-                KzerZaA1.launch_sound = None
