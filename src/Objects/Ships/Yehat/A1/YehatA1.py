da9aa0b8baf10572c130a8eed63a1415445968c6
Murtaza Amin <murtazaamin1@gmail.com>
2024-12-26 12:33:53 -0700

    Revised YehatA1

--- src/Objects/Ships/Yehat/A1/YehatA1.py
+++ src/Objects/Ships/Yehat/A1/YehatA1.py
@@ -1,6 +1,27 @@
 from src.Objects.Ships.ability import Ability, ABILITIES_DATA
+import src.const as const
+import math
 
 class YehatA1(Ability):
-    def __init__(self, parent):
+    def __init__(self, parent,  offset = 0):
         super().__init__("YehatA1", parent)
         ability_data = ABILITIES_DATA["YehatA1"]
+        self.place_self(offset)
+
+    def place_self(self, offset):
+        angle_rad = math.radians(self.parent.rotation)
+        spawn_distance = const.PROJ_GAP + (self.size[1] + self.parent.size[1]) / 2
+        offset_x = math.cos(angle_rad) * offset
+        offset_y = math.sin(angle_rad) * offset
+
+        self.position = [
+            self.parent.position[0] + math.sin(angle_rad) * spawn_distance + offset_x,
+            self.parent.position[1] - math.cos(angle_rad) * spawn_distance + offset_y
+        ]
+        self.heading = self.parent.heading
+        self.rotation = self.parent.rotation
+        angle_rad = math.radians(self.rotation)
+        self.velocity = [
+            math.sin(angle_rad) * self.speed + self.parent.velocity[0] * self.parent_vel,
+            -math.cos(angle_rad) * self.speed + self.parent.velocity[1] * self.parent_vel
+        ]
--- src/Objects/Ships/Yehat/Yehat.py
+++ src/Objects/Ships/Yehat/Yehat.py
@@ -13,35 +13,16 @@ class Yehat(SpaceShip):
         if self.can_action1():
             self.current_energy -= self.a1_cost
             self.action1_timer = int(self.a1_wait * const.ACTION_WAIT_SCALE)
-
-            angle_rad = math.radians(self.rotation)
             side_offset = self.size[0] / 2
-
             projectiles = []
+
             for offset in [-side_offset, side_offset]:
-                projectile = YehatA1(self)
+                ability_obj = YehatA1(self, offset)
+                projectiles.append(ability_obj)
 
-                spawn_distance = const.PROJ_GAP + (self.size[1] + projectile.size[1]) / 2
-                offset_x = math.cos(angle_rad) * offset
-                offset_y = math.sin(angle_rad) * offset
-
-                projectile.position = [
-                    self.position[0] + math.sin(angle_rad) * spawn_distance + offset_x,
-                    self.position[1] - math.cos(angle_rad) * spawn_distance + offset_y
-                ]
-                projectile.heading = self.heading
-                projectile.rotation = self.rotation
-                projectile.velocity = [
-                    math.sin(angle_rad) * projectile.speed + self.velocity[0] * projectile.parent_vel,
-                    -math.cos(angle_rad) * projectile.speed + self.velocity[1] * projectile.parent_vel
-                ]
-
-                if projectile.launch_sound: projectile.launch_sound.play()
-                projectiles.append(projectile)
-
+            if ability_obj.launch_sound: ability_obj.launch_sound.play()
             return projectiles
         return None
-
 
     def perform_action2(self):
         if self.can_action2():
