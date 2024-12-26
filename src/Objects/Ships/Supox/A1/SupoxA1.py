662fd27c63d7f0373c92c6e20eaddd7aee84f857
Murtaza Amin <murtazaamin1@gmail.com>
2024-12-26 11:51:26 -0700

    Revised SupoxA1

--- src/Objects/Ships/Supox/A1/SupoxA1.py
+++ src/Objects/Ships/Supox/A1/SupoxA1.py
@@ -1,6 +1,24 @@
 from src.Objects.Ships.ability import Ability, ABILITIES_DATA
+import src.const as const
+import math
 
 class SupoxA1(Ability):
     def __init__(self, parent):
         super().__init__("SupoxA1", parent)
         ability_data = ABILITIES_DATA["SupoxA1"]
+        self.place_self()
+
+    def place_self(self):
+        angle_rad = math.radians(self.parent.rotation)
+        spawn_distance = const.PROJ_GAP + (self.size[1] + self.parent.size[1]) / 2
+        self.position = [
+            self.parent.position[0] + math.sin(angle_rad) * spawn_distance,
+            self.parent.position[1] - math.cos(angle_rad) * spawn_distance
+        ]
+        self.heading = self.parent.heading
+        self.rotation = self.parent.rotation
+        angle_rad = math.radians(self.rotation)
+        self.velocity = [
+            math.sin(angle_rad) * self.speed + self.parent.velocity[0] * self.parent_vel,
+            -math.cos(angle_rad) * self.speed + self.parent.velocity[1] * self.parent_vel
+        ]
--- src/Objects/Ships/Supox/Supox.py
+++ src/Objects/Ships/Supox/Supox.py
@@ -1,7 +1,6 @@
 from src.Objects.Ships.space_ship import SpaceShip, SHIPS_DATA
 from src.Objects.Ships.Supox.A1.SupoxA1 import SupoxA1
 import src.const as const
-import math
 
 
 class Supox(SpaceShip):
@@ -13,26 +12,9 @@ class Supox(SpaceShip):
         if self.can_action1():
             self.current_energy -= self.a1_cost
             self.action1_timer = int(self.a1_wait * const.ACTION_WAIT_SCALE)
-
-            angle_rad = math.radians(self.rotation)
-
-            projectile = SupoxA1(self)
-
-            spawn_distance = const.PROJ_GAP + (self.size[1] + projectile.size[1]) / 2
-            projectile.position = [
-                self.position[0] + math.sin(angle_rad) * spawn_distance,
-                self.position[1] - math.cos(angle_rad) * spawn_distance
-            ]
-            projectile.heading = self.heading
-            projectile.rotation = self.rotation
-            angle_rad = math.radians(self.rotation)
-            projectile.velocity = [
-                math.sin(angle_rad) * projectile.speed + self.velocity[0] * projectile.parent_vel,
-                -math.cos(angle_rad) * projectile.speed + self.velocity[1] * projectile.parent_vel
-            ]
-
-            if projectile.launch_sound: projectile.launch_sound.play()
-            return projectile
+            ability_obj = SupoxA1(self)
+            if ability_obj.launch_sound: ability_obj.launch_sound.play()
+            return ability_obj
         return None
 
     def perform_action2(self):
