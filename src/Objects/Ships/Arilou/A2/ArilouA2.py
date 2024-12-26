63fc61de3bcd7d9088eee33f13865bcdb9bb4610
Murtaza Amin <murtazaamin1@gmail.com>
2024-12-26 13:12:41 -0700

    Revised ArilouA2

--- src/Objects/Ships/Arilou/A2/ArilouA2.py
+++ src/Objects/Ships/Arilou/A2/ArilouA2.py
@@ -1,9 +1,15 @@
 from src.Objects.Ships.ability import Ability, ABILITIES_DATA
-import pygame
-import math
-import src.const as const
 
 class ArilouA2(Ability):
     def __init__(self, parent):
         super().__init__("ArilouA2", parent)
         ability_data = ABILITIES_DATA["ArilouA2"]
+        self.place_self()
+
+    def place_self(self):
+        self.position = self.parent.position.copy()
+        self.heading = 0
+        self.rotation = 0
+        self.velocity = [0, 0]
+
+
--- src/Objects/Ships/Arilou/Arilou.py
+++ src/Objects/Ships/Arilou/Arilou.py
@@ -13,25 +13,18 @@ class Arilou(SpaceShip):
         if self.can_action1():
             self.current_energy -= self.a1_cost
             self.action1_timer = int(self.a1_wait * const.ACTION_WAIT_SCALE)
-
-            projectile = ArilouA1(self)
-
-            if projectile.launch_sound: projectile.launch_sound.play()
-            return projectile
+            ability_obj = ArilouA1(self)
+            if ability_obj.launch_sound: ability_obj.launch_sound.play()
+            return ability_obj
         return None
 
     def perform_action2(self):
         if self.can_action2():
             self.current_energy -= self.a2_cost
             self.action2_timer = int(self.a2_wait * const.ACTION_WAIT_SCALE)
-
             projectile = ArilouA2(self)
-            projectile.position = self.position.copy()
-            projectile.velocity = [0, 0]
-            projectile.can_collide = False
             self.position[0] = random.randint(0, const.ARENA_SIZE)
             self.position[1] = random.randint(0, const.ARENA_SIZE)
-
             if projectile.launch_sound: projectile.launch_sound.play()
             return projectile
         return None
