3dd781f8f958232c4963a17f121e627d8ffd561b
Murtaza Amin <murtazaamin1@gmail.com>
2024-12-26 12:42:08 -0700

    Revised ZoqFotA1

--- src/Objects/Ships/ZoqFot/A1/ZoqFotA1.py
+++ src/Objects/Ships/ZoqFot/A1/ZoqFotA1.py
@@ -1,6 +1,32 @@
 from src.Objects.Ships.ability import Ability, ABILITIES_DATA
+import src.const as const
+import math, random
 
 class ZoqFotA1(Ability):
     def __init__(self, parent):
         super().__init__("ZoqFotA1", parent)
         ability_data = ABILITIES_DATA["ZoqFotA1"]
+        self.SPREAD_ANGLE = ability_data.get("SPREAD_ANGLE", 3)
+        self.place_self()
+
+    def place_self(self):
+
+        direction = random.randint(-1, 1)
+        angle_offset = direction * self.SPREAD_ANGLE
+        angle_rad = math.radians(self.parent.rotation + angle_offset)
+
+        spawn_distance = const.PROJ_GAP + (self.size[1] + self.parent.size[1]) / 2
+
+        self.position = [
+            self.parent.position[0] + math.sin(angle_rad) * spawn_distance,
+            self.parent.position[1] - math.cos(angle_rad) * spawn_distance
+        ]
+
+        self.heading = 0
+        self.rotation = self.parent.rotation + angle_offset
+
+        angle_rad = math.radians(self.rotation)
+        self.velocity = [
+            math.sin(angle_rad) * self.speed + self.parent.velocity[0] * self.parent_vel,
+            -math.cos(angle_rad) * self.speed + self.parent.velocity[1] * self.parent_vel
+        ]
--- src/Objects/Ships/ZoqFot/ZoqFot.py
+++ src/Objects/Ships/ZoqFot/ZoqFot.py
@@ -1,43 +1,20 @@
 from src.Objects.Ships.space_ship import SpaceShip, SHIPS_DATA
 from src.Objects.Ships.ZoqFot.A1.ZoqFotA1 import ZoqFotA1
 import src.const as const
-import math
-import random
 
 
 class ZoqFot(SpaceShip):
     def __init__(self, ship_name, player_num):
         super().__init__(ship_name, player_num)
         ship_data = SHIPS_DATA[ship_name]
-        self.spread_angle = 3  # Degrees of spread for off-center shots
 
     def perform_action1(self):
         if self.can_action1():
             self.current_energy -= self.a1_cost
             self.action1_timer = int(self.a1_wait * const.ACTION_WAIT_SCALE)
-
-            # Randomly choose direction: -1 for left, 0 for center, 1 for right
-            direction = random.randint(-1, 1)
-            angle_offset = direction * self.spread_angle
-
-            angle_rad = math.radians(self.rotation + angle_offset)
-            projectile = ZoqFotA1(self)
-
-            spawn_distance = const.PROJ_GAP + (self.size[1] + projectile.size[1]) / 2
-            projectile.position = [
-                self.position[0] + math.sin(angle_rad) * spawn_distance,
-                self.position[1] - math.cos(angle_rad) * spawn_distance
-            ]
-            projectile.heading = 0
-            projectile.rotation = angle_offset
-
-            projectile.velocity = [
-                math.sin(angle_rad) * projectile.speed + self.velocity[0] * projectile.parent_vel,
-                -math.cos(angle_rad) * projectile.speed + self.velocity[1] * projectile.parent_vel
-            ]
-
-            if projectile.launch_sound: projectile.launch_sound.play()
-            return projectile
+            ability_obj = ZoqFotA1(self)
+            if ability_obj.launch_sound: ability_obj.launch_sound.play()
+            return ability_obj
         return None
 
     def perform_action2(self):
