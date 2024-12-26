61ed69061f993bc3a369523a1654eab0e1f288bc
Murtaza Amin <murtazaamin1@gmail.com>
2024-12-26 11:31:33 -0700

    Revised ShofixtiA1

--- src/Objects/Ships/Shofixti/Shofixti.py
+++ src/Objects/Ships/Shofixti/Shofixti.py
@@ -13,26 +13,9 @@ class Shofixti(SpaceShip):
         if self.can_action1():
             self.current_energy -= self.a1_cost
             self.action1_timer = int(self.a1_wait * const.ACTION_WAIT_SCALE)
-
-            angle_rad = math.radians(self.rotation)
-
-            projectile = ShofixtiA1(self)
-
-            spawn_distance = const.PROJ_GAP + (self.size[1] + projectile.size[1]) / 2
-            projectile.position = [
-                self.position[0] + math.sin(angle_rad) * spawn_distance,
-                self.position[1] - math.cos(angle_rad) * spawn_distance
-            ]
-            projectile.heading = self.heading
-            projectile.rotation = self.rotation
-            angle_rad = math.radians(self.rotation)
-            projectile.velocity = [
-                math.sin(angle_rad) * projectile.speed + self.velocity[0] * projectile.parent_vel,
-                -math.cos(angle_rad) * projectile.speed + self.velocity[1] * projectile.parent_vel
-            ]
-
-            if projectile.launch_sound: projectile.launch_sound.play()
-            return projectile
+            ability_obj = ShofixtiA1(self)
+            if ability_obj.launch_sound: ability_obj.launch_sound.play()
+            return ability_obj
         return None
 
     def perform_action2(self):
