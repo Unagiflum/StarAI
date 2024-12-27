a39510317f255aaa1e77e989acdf178d14a30d1e
Murtaza Amin <murtazaamin1@gmail.com>
2024-12-27 16:35:07 -0700

    Added MyconA2

---
+++ src/Objects/Ships/Mycon/A2/MyconA2.py
@@ -0,0 +1,7 @@
+from src.Objects.Ships.ability import Ability, ABILITIES_DATA
+
+class MyconA2(Ability):
+    def __init__(self, parent):
+        super().__init__("MyconA2", parent)
+        ability_data = ABILITIES_DATA["MyconA2"]
+        self.HP_GAIN = ability_data.get("HP_GAIN", 4)
--- src/Objects/Ships/Mycon/Mycon.py
+++ src/Objects/Ships/Mycon/Mycon.py
@@ -1,5 +1,6 @@
 from src.Objects.Ships.space_ship import SpaceShip, SHIPS_DATA
 from src.Objects.Ships.Mycon.A1.MyconA1 import MyconA1
+from src.Objects.Ships.Mycon.A2.MyconA2 import MyconA2
 import src.const as const
 
 
@@ -18,10 +19,12 @@ class Mycon(SpaceShip):
         return None
 
     def perform_action2(self):
-        if self.can_action2():
+        if self.can_action2() and self.current_hp < self.max_hp:
             self.current_energy -= self.a2_cost
             self.action2_timer = int(self.a2_wait * const.ACTION_WAIT_SCALE)
-            return None
+            ability_obj = MyconA2(self)
+            self.current_hp = min(self.current_hp + ability_obj.HP_GAIN, self.max_hp)
+            if ability_obj.launch_sound: ability_obj.launch_sound.play()
         return None
 
     def perform_action3(self):
