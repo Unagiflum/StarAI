367c4b5d8f6634c4c53e0a27a65ea0a25bd6a45a
Murtaza Amin <murtazaamin1@gmail.com>
2024-12-27 15:55:38 -0700

    Updated KohrAhA2.py. Now working as expected.

--- src/Objects/Ships/KohrAh/A2/KohrAhA2.py
+++ src/Objects/Ships/KohrAh/A2/KohrAhA2.py
@@ -3,23 +3,24 @@ import src.const as const
 import math
 
 class KohrAhA2(Ability):
-    def __init__(self, parent):
+    def __init__(self, parent, angle_offset = 0):
         super().__init__("KohrAhA2", parent)
         ability_data = ABILITIES_DATA["KohrAhA2"]
         self.DMG_TO_PROJ = ability_data.get("DMG_TO_PROJ", 50)
-        self.QUANTITY = ability_data.get("QUANTITY", 16)
-        self.place_self()
+        self.place_self(angle_offset)
 
-    def place_self(self):
-        angle_rad = math.radians(self.parent.rotation)
+    def place_self(self, angle_offset):
+        angle_rad = math.radians(self.parent.rotation + angle_offset)
         spawn_distance = const.PROJ_GAP + (self.size[1] + self.parent.size[1]) / 2
+
         self.position = [
             self.parent.position[0] + math.sin(angle_rad) * spawn_distance,
             self.parent.position[1] - math.cos(angle_rad) * spawn_distance
         ]
-        self.heading = self.parent.heading
-        self.rotation = self.parent.rotation
-        angle_rad = math.radians(self.rotation)
+
+        self.heading = 0
+        self.rotation = 0
+
         self.velocity = [
             math.sin(angle_rad) * self.speed + self.parent.velocity[0] * self.parent_vel,
             -math.cos(angle_rad) * self.speed + self.parent.velocity[1] * self.parent_vel
--- src/Objects/Ships/KohrAh/KohrAh.py
+++ src/Objects/Ships/KohrAh/KohrAh.py
@@ -9,7 +9,9 @@ class KohrAh(SpaceShip):
     def __init__(self, ship_name, player_num):
         super().__init__(ship_name, player_num)
         ship_data = SHIPS_DATA[ship_name]
-        self.MAX_PROJECTILES = ship_data.get("MAX_PROJECTILES", 8)
+        self.SAW_COUNT = ship_data.get("SAW_COUNT", 8)
+        self.GAS_COUNT = ship_data.get("GAS_COUNT", 16)
+        self.angle_increment = 360 / self.GAS_COUNT
         self.active_projectiles = []
         self.last_action1_state = False
 
@@ -26,7 +28,7 @@ class KohrAh(SpaceShip):
             self.action1_timer = int(self.a1_wait * const.ACTION_WAIT_SCALE)
 
             # Remove oldest projectile if at max
-            if len(self.active_projectiles) >= self.MAX_PROJECTILES:
+            if len(self.active_projectiles) >= self.SAW_COUNT:
                 oldest = self.active_projectiles.pop(0)
                 oldest.currently_alive = False
 
@@ -51,9 +53,16 @@ class KohrAh(SpaceShip):
         if self.can_action2():
             self.current_energy -= self.a2_cost
             self.action2_timer = int(self.a2_wait * const.ACTION_WAIT_SCALE)
-            ability_obj = KohrAhA2(self)
+
+            projectiles = []
+
+            for ii in range(self.GAS_COUNT):
+                angle_offset = ii * self.angle_increment
+                ability_obj = KohrAhA2(self, angle_offset)
+                projectiles.append(ability_obj)
+
             if ability_obj.launch_sound: ability_obj.launch_sound.play()
-            return ability_obj
+            return projectiles
         return None
 
     def perform_action3(self):
